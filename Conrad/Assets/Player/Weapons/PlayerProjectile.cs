6aa719d68a50281e149080b37b9d32ccf83f5a69
Cam <141291949+MDS00002OW@users.noreply.github.com>
2023-10-27 08:11:31 +1300

    Bullet Mask Deleting
    Added code to delete Bullet Mask after the bullet has fired.  Operates on a timer since mask does not trigger condition otherwise.
    - Also altered Scene slightly- change there unimportant.

--- Conrad/Assets/Player/Weapons/PlayerProjectile.cs
+++ Conrad/Assets/Player/Weapons/PlayerProjectile.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class PlayerProjectile : MonoBehaviour
 {
+    private void Start()
+    {
+        //Sets a Bullet Lifetime of 3 seconds.
+        StartCoroutine(DestroyAfterDelay(3f));
+    }
 
+    private IEnumerator DestroyAfterDelay(float delay)
+    {
+        //Deletes object after 'delay' seconds.
+        //Used to delete BulletMask as it doesn't collide
+        yield return new WaitForSeconds(delay);
+        Destroy(gameObject);
+    }
     
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        //Destroys bullet on collision/
         Destroy(gameObject);
     }
 }
