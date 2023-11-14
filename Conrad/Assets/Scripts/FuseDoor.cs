84aa2f3d6dd1938eea31dcf66e7c84ee2469e16f
Patrick Gleeson <130936504+WarTurtleMedia@users.noreply.github.com>
2023-11-14 18:35:14 +1300

    + Door Anim + SOME BUGS DEAL WITH IT

--- Conrad/Assets/Scripts/FuseDoor.cs
+++ Conrad/Assets/Scripts/FuseDoor.cs
@@ -7,31 +7,72 @@ public class FuseDoor : MonoBehaviour
 
     public FuseBox m_FuseBoxRef; //Fuse Box Ref 
     public bool m_IsDoorOpen;
-    public bool m_DoorDefaultState; //Sets the Doors Default State : TRUE = ACTIVE // FALSE = INACTIVE
+    public bool m_DoorDefaultState; //Sets the Doors Default State : TRUE = ACTIVE // FALSE = INACTIVE'
+    public bool m_IsFirstTime = true;
+
+    
 
     // Start is called before the first frame update
     void Start()
     {
         m_IsDoorOpen = m_DoorDefaultState; //Setting the Doors Current State to the Default 
+        gameObject.GetComponent<Animator>().SetBool("IsOpen", m_IsDoorOpen);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (m_FuseBoxRef.isFuseActive == true) //Checking if the Fuse is Active 
+        if (m_FuseBoxRef.isFuseActive == true ) //Checking if the Fuse is Active 
         {
-            m_FuseBoxRef.m_FuseState = FuseState.State_Active; //Setting the Fuse State 
-            gameObject.GetComponent<BoxCollider2D>().enabled = !m_DoorDefaultState; //Setting the Collider to the Oppisite of the Default State
-            gameObject.GetComponent<SpriteRenderer>().enabled = !m_DoorDefaultState; //Temp remove the sprite 
-            m_IsDoorOpen = !m_IsDoorOpen; //Setting the Current State of the Door 
+            
+            //if (gameObject.name == "FuseDoorYellow#1")
+            //{
+
+               
+                                                                                        //gameObject.GetComponent<SpriteRenderer>().enabled = !m_DoorDefaultState; //Temp remove the sprite 
+                //m_IsDoorOpen = !m_IsDoorOpen; //Setting the Current State of the Door 
+
+                if (m_IsFirstTime == true)
+                {
+                    m_IsDoorOpen = !m_IsDoorOpen; //Setting the Current State of the Door 
+                    Debug.Log("DOOR IS OPEN");
+                //gameObject.GetComponent<Animator>().SetTrigger("DoorOpen");
+                    m_FuseBoxRef.m_FuseState = FuseState.State_Active; //Setting the Fuse State 
+                    gameObject.GetComponent<BoxCollider2D>().enabled = !m_DoorDefaultState; //Setting the Collider to the Oppisite of the Default State
+
+                    gameObject.GetComponent<Animator>().SetBool("IsOpen", m_IsDoorOpen);
+                    m_IsFirstTime = false;
+                }
+            
+            //}
+       
+
+
+           
         }
         else if (m_FuseBoxRef.isFuseActive == false)
         {
-            m_FuseBoxRef.m_FuseState = FuseState.State_Inactive; //Setting the Fuse State 
-            gameObject.GetComponent<BoxCollider2D>().enabled = m_DoorDefaultState;
-            gameObject.GetComponent<SpriteRenderer>().enabled = m_DoorDefaultState;
-            m_IsDoorOpen = !m_IsDoorOpen; //Setting the Current State of the Door 
+
+
+            if (m_IsFirstTime == false)
+            {
+                m_IsDoorOpen = !m_IsDoorOpen; //Setting the Current State of the Door 
+                m_FuseBoxRef.m_FuseState = FuseState.State_Inactive; //Setting the Fuse State 
+                gameObject.GetComponent<BoxCollider2D>().enabled = m_DoorDefaultState;
+                m_IsFirstTime = true;
+            }
+
+
+            //if (gameObject.name == "FuseDoorYellow#1")
+            //{
+
+                gameObject.GetComponent<Animator>().SetBool("IsOpen", m_IsDoorOpen);
+
+            //}
+
+            
         }
+        
       
 
        
